bb1380a25e0e6eb4c8b728610450a9914a2335f4
omija <oomija@tenso.com>
2018-06-26 19:02:00 +0900

    コメントをRDoc基準に変更 変数名から動詞を削除

--- linear-search.rb
+++ linear-search.rb
@@ -1,22 +1,25 @@
-# ==== メソッド名
-# linear_search(線形探索用メソッド: strings)
+# ==== メソッド内容
+# * 線形探索を行う。
 # ==== 引数
-# find_number(検索値: integer)
-# find_renge(検索範囲: array)
+# * number :: 検索値: integer
+# * renge :: 検索範囲: array
 # ==== 変数
-# find_result(検索判定: array)
+# * result :: 検索判定: array
+#--
 # ==== 処理内容
-# each (find_range から find_number を探して、結果を find_result に格納: array)
-# if_true (find_result が nil ならば、検索値が見つかった旨を返す: strings)
-# if_false (find_result がレシーバ自身なら、検索値が見つからなかった旨を返す: strings)
-def linear_search(find_number, find_range)
-  find_result = find_range.each { |n| break if n == find_number }
-  if find_result.nil?
-    puts "#{find_number}はありました"
+# each (range から number を探して、結果を result に格納: array)
+# if_true (result が nil ならば、検索値が見つかった旨を返す: strings)
+# if_false (result がレシーバ自身なら、検索値が見つからなかった旨を返す: strings)
+#++
+
+def linear_search(number, range)
+  result = range.each { |n| break if n == number }
+  if result.nil?
+    puts "#{number}はありました"
   else
-    puts "#{find_number}はありませんでした"
+    puts "#{number}はありませんでした"
   end
 end
 
-# 出力
+# 出力 :nodoc:
 linear_search(101, [*1..100])
