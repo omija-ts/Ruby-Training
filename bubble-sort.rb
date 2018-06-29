6cd84522de14e3d4ed05fbeb1fd41f1e77b5221c
omija <oomija@tenso.com>
2018-06-29 18:31:16 +0900

    ソート済みの場合、処理を抜けるコードを記載

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -10,17 +10,22 @@
 # ==== 処理内容
 # for で配列の要素数を回す、1回の処理ごとにループの数を減らしていく
 # ネスト内で要素同士を比べ、正の場合は number に一時的に数値を格納し入れ替える。
+# counter の処理しなかった回数と range の値が一緒なら、ソート済みとしてループを抜ける。
 #++
 
-def bubble_sort(range, flag = 1) 
+def bubble_sort(range, flag = 1)
   for i in 1...range.size
+    counter = 0
     for j in 0..(range.size - i)
       if (range[j] <=> range[j + 1]) == flag
         number = range[j]
         range[j] = range[j + 1]
         range[j + 1] = number 
+      else
+         counter += 1
       end
     end
+    return "ソート済みでした" if counter == range.size
   end
   range
 end
@@ -28,4 +33,4 @@ end
 # 出力 :nodoc:
 my_range = [*0..100].shuffle.take(10)
 puts "#{my_range}:元データ"
-p bubble_sort(my_range)
+p bubble_sort([*1..10])
