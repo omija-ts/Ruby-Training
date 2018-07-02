a7221032e4ae01639a7abae4e2722b957f0519af
omija <oomija@tenso.com>
2018-07-02 10:33:51 +0900

    コードを読み易いように変更

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -8,24 +8,23 @@
 # * number :: 数値格納用: integer
 #--
 # ==== 処理内容
-# for で配列の要素数を回す、1回の処理ごとにループの数を減らしていく
+# for で配列の要素数を回す、1回の処理ごとに i でループの数を減らしていく
 # ネスト内で要素同士を比べ、正の場合は number に一時的に数値を格納し入れ替える。
-# counter の処理しなかった回数と range の値が一緒なら、ソート済みとしてループを抜ける。
+# counter に range.size の値を入れ込み、ループ後に変化がなければソート済みとして return する。
 #++
 
 def bubble_sort(range, flag = 1)
   for i in 1...range.size
-    counter = 0
+    counter = (range.size - i)
     for j in 0..(range.size - i)
       if (range[j] <=> range[j + 1]) == flag
         number = range[j]
         range[j] = range[j + 1]
         range[j + 1] = number 
-      else
-         counter += 1
+        counter = 0
       end
     end
-  return range if (counter - 1) == (range.size - i)
+    return range if counter == (range.size - i)
   end
   range
 end
